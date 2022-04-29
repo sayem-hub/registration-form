8e92d20ca0c7dcf516b93a43a58bcac9990a36ae
Abu Sayem Amir <sayem74@gmail.com>
2022-04-29 16:29:33 +0600

    completed making rigstration form

--- form-data.php
+++ form-data.php
@@ -1,12 +1,16 @@
 <?php 
 
-    $full_name   = $_POST['full_name'];
-    $phone            = $_POST['phone'];
+    $full_name       = $_POST['full_name'];
+    $phone              = $_POST['phone'];
     $email               = $_POST['email'];
-    $department     = $_POST['department'];
-    $section    = $_POST['section'];
-    $company    = $_POST['company'];
+    $department      = $_POST['department'];
+    $section             = $_POST['section'];
+    $company          = $_POST['company'];
 
-        if ()
+        if ($full_name == '' || $phone == '' || $email == '' || $department == '' || $section == '' || $company == '') {
+            echo "All fields are required!";
+        } else {
+            echo "Everything is Okay!";
+        }
 
 ?>
