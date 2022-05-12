7c63bd7f6a55808b836f8486e5eeeb489bb773d3
Abu Sayem Amir <76561416+sayem-hub@users.noreply.github.com>
2022-05-12 19:05:13 +0600

    making registration form validation

--- form-data.php
+++ form-data.php
@@ -1,16 +1,63 @@
-<?php 
 
-    $full_name       = $_POST['full_name'];
-    $phone              = $_POST['phone'];
-    $email               = $_POST['email'];
-    $department      = $_POST['department'];
-    $section             = $_POST['section'];
-    $company          = $_POST['company'];
+	<?php
 
-        if ($full_name == '' || $phone == '' || $email == '' || $department == '' || $section == '' || $company == '') {
-            echo "All fields are required!";
-        } else {
-            echo "Everything is Okay!";
-        }
+function validate($message, $msgAlert = "danger")
+{
+    return "<div class=\"alert alert-{$msgAlert} alert-dismissible fade show\" role=\"alert\">
+                {$message}!
+                <button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"alert\" aria-label=\"Close\"></button>
+            </div>";
+}
+
+/**
+ * Function validEmail for check email valid or not.
+ */
+function validEmail($email)
+{
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+/**
+ * Function eduMail for filtering institution email addresses
+ */
+function eduMail($email)
+{
+    $valid_emails = array('uttarauniversity.edu.bd', 'diu.edu.bd', 'ewubd.edu', 'brac.edu.bd', 'nsu.edu.bd');
+    $email_arr = explode('@', $email, 2);
+    if (in_array(end($email_arr), $valid_emails)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+ if (isset($_POST['submit'])) {
+		 $full_name       = $_POST['full_name'];
+		 $phone              = $_POST['phone'];
+	$email               = $_POST['email'];
+	$department      = $_POST['department'];
+	$section             = $_POST['section'];
+	$company          = $_POST['company'];
+
+		if (empty($full_name) || empty($phone) || empty($email) || empty($department) || empty($section)  || empty($company) ) {
+			$validation_msg = validate('All fields are required');
+
+		} else {
+			$validation_msg = validate('Everything okay', 'success');
+
+		}
+}
 
 ?>
+
+
+
+
+
+
+
--- registration-form.php
+++ registration-form.php
@@ -8,20 +8,91 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <title>Document</title>
+    <title>Registration Form</title>
 </head>
 
 <body>
+      <?php
+          function validate($message, $msgAlert = "danger") {
+
+          return "<div class=\"alert alert-{$msgAlert} alert-dismissible fade show\" role=\"alert\">
+                      {$message}!
+                      <button type=\"button\" class=\"btn-close\" data-bs-dismiss=\"alert\" aria-label=\"Close\"></button>
+                  </div>";
+      }
+
+      /**
+       * Function validEmail for check email valid or not.
+       */
+      function validEmail($email) {
+
+          if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+              return true;
+          } else {
+              return false;
+          }
+      }
+
+      /**
+       * Function eduMail for filtering institution email addresses
+       */
+      function eduMail($email) {
+
+          $valid_emails = array('uttarauniversity.edu.bd', 'diu.edu.bd', 'ewubd.edu', 'brac.edu.bd', 'nsu.edu.bd');
+          $email_arr = explode('@', $email, 2);
+          if (in_array(end($email_arr), $valid_emails)) {
+              return true;
+           } else {
+              return false;
+          }
+      }
+
+      if (isset($_POST['submit'])) {
+          $full_name = $_POST['full_name'];
+          $phone = $_POST['phone'];
+          $email_address = $_POST['email_address'];
+          $department = $_POST['department'];
+          $section = $_POST['section'];
+          $company = $_POST['company'];
 
 
+          if (empty($full_name) || empty($phone) || empty($email_address) || empty($department) || empty($section) || empty($company)) {
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-3"></div>
-            <div class="col-md-6">
-                <h3 class="text-center mt-3">Registration Form</h3>
+              $validation_msg = validate('All fields are required');
 
-                <form action="form-data.php" method="POST">
+          }  elseif (validEmail($email_address) == false) {
+
+                $validation_msg = validate('Email is not valid', 'warning');
+
+            } elseif (eduMail($email_address) == false) {
+
+                $validation_msg = validate('Email is not a edu mail', 'warning');
+
+
+          } else {
+              $validation_msg = validate('Everything okay', 'success');
+
+          }
+      }
+
+
+      ?>
+
+
+          <div class="container">
+              <div class="row">
+                  <div class="col-md-3"></div>
+                  <div class="col-md-6">
+                      <h3 class="text-center mt-3">Registration Form</h3>
+
+                          <?php
+                            if (isset($validation_msg)) {
+                                echo $validation_msg;
+                            }
+
+                          ?>
+
+                <form action="" method="POST">
 
                     <div class="mb-3">
                         <label for="name" class="form-label">Full Name</label>
@@ -35,9 +106,9 @@
                     </div>
 
                     <div class="mb-3">
-                        <label for="email" class="form-label">Email address</label>
-                        <input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp"
-                            placeholder="Enter Your Email...">
+                        <label for="email_address" class="form-label">Email address</label>
+                        <input type="text" name="email_address" class="form-control" id="email_address" placeholder="Enter Your Email...">
+
                     </div>
 
                     <div class="mb-3">
@@ -67,7 +138,7 @@
                         </select>
                     </div>
 
-                    <button type="submit" class="btn btn-primary">Register</button>
+                    <button type="submit" class="btn btn-primary"  name="submit">Register</button>
 
                     <p class="text-center">Have an account? <a href="">Log In</a> </p>
 
@@ -76,9 +147,8 @@
         </div>
     </div>
 
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-
-
 </body>
 
 </html>
